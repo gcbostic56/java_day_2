7d39bca61e0cbce94ad9cce3d53523007c5ec09c
Cameron <bmscameronb072a@yahoo.com>
2022-05-03 11:48:02 -0400

    Indeed

---
+++ Greetings.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main (String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("What's your name?");
+    String name = scanner.nextLine();
+    System.out.println("Hello " + name);
+    
+}
+}
---
+++ HelloThere.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class HelloThere {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello there! How are you doing today?");
+        String one = scanner.nextLine();
+        System.out.println("Wow - interesting! Tell me more!");
+        String two = scanner.nextLine();
+        
+}
